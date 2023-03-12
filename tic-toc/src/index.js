00987ceca0524b13c387a9d2d01c39d51229c801
icivixwalter <47453186+icivixwalter@users.noreply.github.com>
2023-03-12 17:43:16 +0100

    III° commit CLASSE BOARD mantiene lo stato

--- tic-toc/src/index.js
+++ tic-toc/src/index.js
@@ -24,6 +24,15 @@ class Board extends React.Component {
         };
     }
 
+    /*11_HandleClick_in_Square = BOARD mantiene lo stato nei vari componenti
+    * di square */
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+
     /*  @CODICE---> 09_02_Modifico_METODO_RenderSquare = Modifichiamo Board per inviare un PROPS a SQUARE in modo
                         che riceva il valore corrente X, O o null per ogni square, e visto che IL COSTRUTTORE DI BOARD
                         è stato modificato viene anche modificato il METODO RenderSquare di Board facendo si che
@@ -32,19 +41,23 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
-                /*  @CODICE---> 09_04_Modifico_METODO_RenderSquare_PROPS_FUNZIONE = Per informare Board quando uno
+                /*  @CODICE---> 09_03_Modifico_METODO_RenderSquare_PROPS_FUNZIONE = Per informare Board quando uno
                     square è cliccato modifichiamo la funzione Square, con la quale INVIAMO  A Modifichiamo Board per inviare un PROPS a SQUARE in modo
                         che riceva il valore corrente X, O o null per ogni square, e visto che IL COSTRUTTORE DI BOARD
                         è stato modificato viene anche modificato il METODO RenderSquare di Board facendo si che
-                        possa leggere l'array
+                        possa leggere l'array - ATTENZIONE è stato diviso square in piu linee per leggibilita ed
+                        aggiunto le parantesi (); per fare in modo che JavaScript NON INSERISCA IL ; dopo RERTURN per
+                        evitare che il codice non funzioni piu.
+                        N.B. in conclusione BOARD ----> SQUARE passa 2 propos value + on click
     */
                 //@CODICE---> 09_02_stato_in_Square
+                //2 props passate da Board a Square.
                 value={this.state.squares[i]}
+                // props che viene richiamata da Square quando viene cliccata.
                 onClick={() => this.handleClick(i)}
             />
         );
     }
-
 
 
     render() {
@@ -81,24 +94,35 @@ class Square extends React.Component {
     /*HO inserito in costruttore per modificare lo stato, in quanto
     * i props sono proprieta di sola lettura. Quindi se si vuole modificare
     * le props occorre creare un oggetto
-    * */
-    constructor(props) {
-        super(props);
-        this.state = {value: null,};
-    }
+    *
+    * ALTRA MODIFICA
+    * @CODICE---> 10_03_Square_RimossoCostruttore = Square non tiene piu traccia della partita passa a Board
+    *
+    *
+        constructor(props) {
+            super(props);
+            this.state = {value: null,};
+        }
+
+     */
 
 
     //metodo render di square che ritorna un Button + un click + un valore di i
     render() {
         return (
             // definisci l'evento nell'onClick
-
+            //componente BUTTON del DOM di react
             <button
                 className="square"
-
-                onClick={() => this.setState({value: 'X'})}
+                // @CODICE---> 10_02_Square_ModificaDa_state_a_props.onclick
+                // CON QUESTA modifica, si ha l'evento su uno square, che viene cliccato
+                //viene richiamata la funzione OnClick che viene fornita da Board : ... this.props.onClick()}
+                onClick={() => this.props.onClick()}
+                //sospeso = mette le x --->  onClick={() => this.setState({value: 'X'})}
             >
-                {this.state.value}
+                {/*//@CODICE---> 10_01_Square_ModificaDa_state_a_props*/}
+                {/*//---> sospeso {this.state.value}*/}
+                {this.props.value}
             </button>);
     }/*render fine */
 }
